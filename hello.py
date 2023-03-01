3f236de657ac3004aa3b13e4e264e93ffc9e7f2a
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-03-01 10:42:48 -0300

    Classes de formulário

--- hello.py
+++ hello.py
@@ -3,8 +3,12 @@ from datetime import datetime
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "Super segredo de criptografia"
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -28,3 +32,8 @@ def index():
 @app.route("/user/<name>")
 def user(name):
     return render_template("user.html", name=name)
+
+
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
