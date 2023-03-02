3006c8a9db251c151c591f7a2727806f6f05b97d
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-03-02 14:02:19 -0300

    uso do banco de dadosem funções de view

--- hello.py
+++ hello.py
@@ -25,16 +25,27 @@ def index():
     form = NameForm()
 
     if form.validate_on_submit():
-        old_name = session.get('name')
+        user = User.query.filter_by(username=form.name.data).first()
 
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        if user is None:
+            user = User(username=form.name.data)
+
+            db.session.add(user)
+            db.session.commit()
+
+            session['known'] = False
+        else:
+            session['known'] = True
 
         session['name'] = form.name.data
+        form.name.data = ''
 
         return redirect(url_for('index'))
 
-    return render_template("index.html", form=form, name=session.get('name'))
+    return render_template("index.html",
+                           form=form,
+                           name=session.get('name'),
+                           know=session.get('known', False))
 
 
 @app.route("/user/<name>")
@@ -62,7 +73,7 @@ class Role(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
