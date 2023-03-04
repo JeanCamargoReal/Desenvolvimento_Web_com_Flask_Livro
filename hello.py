63cdf7134b278453345392486e0dbf146748bcbd
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-03-04 16:44:22 -0300

    configuração do Flask-Mail para o Gmail

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, redirect, render_template, session, url_for
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
@@ -12,14 +13,23 @@ from wtforms.validators import DataRequired
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+# SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# JWT
 app.config["SECRET_KEY"] = "Super segredo de criptografia"
+# Flask-Mail
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 
 @app.route("/", methods=["GET", "POST"])
